c7e37a03f1a04506ba3656b6df2efb4382da5dcd
羅靖雅 <cynthia2094.lo@gmail.com>
2022-04-06 18:03:13 +0800

    20220406

---
+++ config.js
@@ -0,0 +1,20 @@
+const otherAccontList = {
+  1: ['smhc', '5tgb^YHN'],
+  2: ['smhc.aws@gmail.com', 'smhcwelcome1'],
+  3: ['iAliceadmin', 'iAlice5466'],
+  4: ['15858132388', 'a15858132388'],
+  5: ['192.168.1.176', 'administrator', '5tgb^YHN'],
+}
+const his3AccontList = {
+  1: ['oer', '1qaz@WSX'],
+  2: ['891003', '10031003'],
+  3: ['community', '977016'],
+  4: ['141006', '123456'],
+  5: ['N201', '123456'],
+}
+function getOther(){
+  return otherAccontList
+}
+function getHis3(){
+  return his3AccontList
+}
